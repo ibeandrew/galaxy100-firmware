8b987f2d5cbb7b4f945d99bc567514192e38c03a
ibeandrew <andjamoo@gmail.com>
2026-05-02 08:56:31 -0600

    Add custom linker script: app at 0x08008000 (past RDMCTMZT bootloader)

--- keyboards/epomaker/galaxy100/post_rules.mk
+++ keyboards/epomaker/galaxy100/post_rules.mk
@@ -1,3 +1,6 @@
 RGB_MATRIX_CUSTOM_USER = yes
 include keyboards/epomaker/galaxy100/rgb_record/rgb_record.mk
 
+# Use our custom linker script with app at 0x08008000 (skipping RDMCTMZT bootloader region)
+MCU_LDSCRIPT = WB32FQ95-app
+
